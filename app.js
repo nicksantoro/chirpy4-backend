c58fc1dd407e26815a1ce065583c8006ff72dd66
Mansoor Bahramand <mansoor.bahramand@galvanize.com>
2019-01-03 14:45:06 -0800

    added a home route for nicks AI robot feature

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 3000;
 
+app.get('/', (request, response) => {
+  response.status(200).send('Hello Nick, I am your new AI working');
+});
+
 
 app.listen(port, () => {
   console.log(`we are live on ${port}`)
