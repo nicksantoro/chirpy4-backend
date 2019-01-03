dc1839f0920025a53d7f939e033396a857dbf6f9
Nick Santoro <santoro.nick@gmail.com>
2019-01-03 14:52:29 -0800

    added git ignore file

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 const port = 3000;
+
+
 
 
 app.listen(port, () => {
   console.log(`we are live on ${port}`)
 })
+
