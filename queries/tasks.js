85738d1c27b8ae4fc09f6e99a0a1a69a0a520c61
Nick Santoro <santoro.nick@gmail.com>
2019-01-07 12:01:33 -0800

    created end point to get task by id

--- queries/tasks.js
+++ queries/tasks.js
@@ -4,6 +4,15 @@ const getAllTasks = () => {
   return knex('tasks').orderBy('created_at', 'desc')
 }
 
+const getTaskById = (taskId) => {
+  return knex('tasks')
+    .where('id', taskId).first()
+    .then(result => {
+      return result;
+    })
+}
+
 module.exports = {
-  getAllTasks
+  getAllTasks,
+  getTaskById
 }
