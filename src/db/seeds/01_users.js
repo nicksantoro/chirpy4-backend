c8b35aaf160025af1962adb3303a345d263c8bce
Nick Santoro <santoro.nick@gmail.com>
2019-01-17 15:17:38 -0800

    seeds error

--- src/db/seeds/01_users.js
+++ src/db/seeds/01_users.js
@@ -9,16 +9,16 @@ exports.seed = function (knex, Promise) {
           username: 'sunnydave',
           password: '$2a$10$3xKOYnwmO7z6IxSSpLKtyOoNgeJKOHvw04sUxBhqO.ar2Kjv8mH26',
           email: 'dave@galvanize.com',
+          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. ",
           image: 'https://66.media.tumblr.com/95817645b3415ac5cc1387b4e76a93e1/tumblr_plbdyvC3iQ1r367cco4_1280.jpg',
-          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. "
         },
         {
           name: 'Elizabeth',
           username: 'biz',
           password: 'password123',
           email: 'elizabeth@email.com',
+          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. ",
           image: "https://66.media.tumblr.com/a61bb36f6e9b9829c84bd688f1545199/tumblr_plbdyvC3iQ1r367cco7_1280.jpg",
-          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. "
 
         },
         {
@@ -26,8 +26,8 @@ exports.seed = function (knex, Promise) {
           username: 'iamthecoolest',
           password: 'password123',
           email: 'john@email.com',
+          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. ",
           image: "https://66.media.tumblr.com/8f1e0162abf95dac56cf45afa1aafe49/tumblr_plbdyvC3iQ1r367cco6_1280.jpg",
-          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. "
 
         },
         {
@@ -35,8 +35,8 @@ exports.seed = function (knex, Promise) {
           username: 'maryme',
           password: 'password123',
           email: 'mary@email.com',
+          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. ",
           image: "https://66.media.tumblr.com/3326cf043780a9167788db2c576dce61/tumblr_plbdyvC3iQ1r367cco2_1280.jpg",
-          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. "
 
         },
         {
@@ -44,8 +44,8 @@ exports.seed = function (knex, Promise) {
           username: 'ilovesf',
           password: '$2a$10$3xKOYnwmO7z6IxSSpLKtyOoNgeJKOHvw04sUxBhqO.ar2Kjv8mH26',
           email: 'isabella@email.com',
+          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. ",
           image: "https://66.media.tumblr.com/a61bb36f6e9b9829c84bd688f1545199/tumblr_plbdyvC3iQ1r367cco7_1280.jpg",
-          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. "
 
         },
         {
@@ -53,8 +53,8 @@ exports.seed = function (knex, Promise) {
           username: 'mdmark',
           password: '$2a$10$3xKOYnwmO7z6IxSSpLKtyOoNgeJKOHvw04sUxBhqO.ar2Kjv8mH26',
           email: 'mark@galvanize.com',
+          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. ",
           image: "https://66.media.tumblr.com/cefe1414e60b6e8ad00b70d92affae6c/tumblr_plbdyvC3iQ1r367cco3_1280.jpg",
-          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. "
 
         },
         {
@@ -62,8 +62,8 @@ exports.seed = function (knex, Promise) {
           username: 'lexilu',
           password: 'password123',
           email: 'alexis@email.com',
+          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. ",
           image: "https://66.media.tumblr.com/a61bb36f6e9b9829c84bd688f1545199/tumblr_plbdyvC3iQ1r367cco7_1280.jpg",
-          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. "
 
         },
         {
@@ -71,8 +71,8 @@ exports.seed = function (knex, Promise) {
           username: 'lisapanda',
           password: 'password123',
           email: 'fox@email.com',
+          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. ",
           image: "https://66.media.tumblr.com/8f1e0162abf95dac56cf45afa1aafe49/tumblr_plbdyvC3iQ1r367cco6_1280.jpg",
-          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. "
 
         },
         {
@@ -80,8 +80,8 @@ exports.seed = function (knex, Promise) {
           username: 'jellybean',
           password: 'password123',
           email: 'linda@email.com',
+          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. ",
           image: "https://66.media.tumblr.com/3326cf043780a9167788db2c576dce61/tumblr_plbdyvC3iQ1r367cco2_1280.jpg",
-          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. "
 
         },
         {
@@ -89,8 +89,8 @@ exports.seed = function (knex, Promise) {
           username: 'lovetopaint',
           password: '$2a$10$3xKOYnwmO7z6IxSSpLKtyOoNgeJKOHvw04sUxBhqO.ar2Kjv8mH26',
           email: 'melissa@email.com',
+          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. ",
           image: "https://66.media.tumblr.com/a61bb36f6e9b9829c84bd688f1545199/tumblr_plbdyvC3iQ1r367cco7_1280.jpg",
-          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. "
 
         },
         {
@@ -98,8 +98,8 @@ exports.seed = function (knex, Promise) {
           username: 'nick3000',
           password: '$2a$10$3xKOYnwmO7z6IxSSpLKtyOoNgeJKOHvw04sUxBhqO.ar2Kjv8mH26',
           email: 'santoro.nick@gmail.com',
+          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. ",
           image: "https://66.media.tumblr.com/cefe1414e60b6e8ad00b70d92affae6c/tumblr_plbdyvC3iQ1r367cco3_1280.jpg",
-          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. "
 
         },
         {
@@ -107,8 +107,8 @@ exports.seed = function (knex, Promise) {
           username: 'happyperson',
           password: 'password123',
           email: 'paula@email.com',
+          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. ",
           image: "https://66.media.tumblr.com/3326cf043780a9167788db2c576dce61/tumblr_plbdyvC3iQ1r367cco2_1280.jpg",
-          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. "
 
         },
         {
@@ -116,8 +116,8 @@ exports.seed = function (knex, Promise) {
           username: 'workinhard',
           password: 'password123',
           email: 'steve@email.com',
+          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. ",
           image: "https://66.media.tumblr.com/8f1e0162abf95dac56cf45afa1aafe49/tumblr_plbdyvC3iQ1r367cco6_1280.jpg",
-          bio: "Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general. "
         }
       ]);
     });
